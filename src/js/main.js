85413b7dc9aebb384ba0fcdc60b19be446be986a
Maryam Afallah <maryamafallah950@gmail.com>
2026-04-25 13:44:38 +0200

    Stap 5
    Js gekoppeld aan DOM [anime kaartjes okk]

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 
+const animeList = document.getElementById("anime-list");
+
 const apiURL = 'https://api.jikan.moe/v4/top/anime';
 
 //functie voor data
@@ -6,12 +8,44 @@ async function haalAnimeOp() {
     try {
         const reactie = await fetch(apiURL);
         const resultaat = await reactie.json();
-        
-        //CHECK
-        console.log("De data is binnen:", resultaat.data);
+
+        animeList.innerHTML = "";
+
+        resultaat.data.forEach(anime => {
+            toonAnime(anime);
+        });
+
     } catch (fout) {
-        console.log("Er is iets misgegaan:", fout);
+        console.log("Fout:", fout);
     }
+}
+
+
+
+//functie anime
+function toonAnime(anime) {
+
+    const card = document.createElement("article");
+    card.classList.add("anime-card");
+
+    card.innerHTML = `
+        <img src="${anime.images.jpg.image_url}">
+
+        <div class="card-content">
+            <h2>${anime.title}</h2>
+
+            <div class="anime-info">
+                <p>Score: ${anime.score}</p>
+                <p>Type: ${anime.type}</p>
+                <p>Afleveringen: ${anime.episodes}</p>
+                <p>Status: ${anime.status}</p>
+            </div>
+        </div>
+
+        <button class="fav-btn">Favoriet</button>
+    `;
+
+    animeList.appendChild(card);
 }
 
 haalAnimeOp();
